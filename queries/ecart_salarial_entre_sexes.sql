f680bf9fe22d6248b5d3dba31837f5ecd8fff025
Edoardo Nasuto <edoardo.nasuto@gmail.com>
2025-04-28 14:52:51 +0200

    refactor: delete "" to sql queries

--- queries/ecart_salarial_entre_sexes.sql
+++ queries/ecart_salarial_entre_sexes.sql
@@ -1,6 +1,5 @@
-SELECT "Employe"."sexe", 
-                ROUND(AVG(CAST("Contrat"."remuneration_h" AS FLOAT)) * 151.67, 2) AS salaire_moyen
-FROM "Contrat"
-JOIN "Employe" ON "Contrat"."id_employe" = "Employe"."id"
-WHERE "Employe"."sexe" IN ('Féminin', 'Masculin')
-GROUP BY "Employe"."sexe"
+SELECT Employe.sexe, ROUND(AVG(CAST(Contrat.remuneration_h AS FLOAT)) * 151.67, 2) AS salaire_moyen
+FROM Contrat
+JOIN Employe ON Contrat.id_employe = Employe.id
+WHERE Employe.sexe IN ('Féminin', 'Masculin')
+GROUP BY Employe.sexe
--- queries/nombre_de_femme_direction.sql
+++ queries/nombre_de_femme_direction.sql
@@ -1,5 +1,5 @@
-SELECT "Employe"."sexe", COUNT(*) AS count 
-FROM "Contrat"
-JOIN "Employe" ON "Contrat"."id_employe" = "Employe"."id"
-WHERE "Contrat"."poste_contrat" = 'direction'
-GROUP BY "Employe"."sexe"
+SELECT Employe.sexe, COUNT(*) AS count 
+FROM Contrat
+JOIN Employe ON Contrat.id_employe = Employe.id
+WHERE Contrat.poste_contrat = 'direction'
+GROUP BY Employe.sexe
